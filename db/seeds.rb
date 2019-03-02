06b9ade473cf82ac0a372a214b925d8a4b37e65c
Mike Madsen <mikewmadsen@gmail.com>
2019-03-01 20:14:47 -0800

    Fix heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,12 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-require 'open-uri'
+# #   Character.create(name: 'Luke', movie: movies.first)
+# require 'open-uri'
 
 demo = User.create({username: "sergeybrin", email: "sergeybrin@nutube.com", password: "demologin"})
 mike = User.create({username: "mike", email: "mike@nutube.com", password: "123456"})
 
-testvid1 = Video.create({title: "testvid1", description: "this is the first test video", length: 50, user_id: 1})
-video1 = File.open('app/assets/images/test_video.mp4')
-testvid1.video.attach(io: video1, filename: 'testvid1')
+# testvid1 = Video.create({title: "testvid1", description: "this is the first test video", length: 50, user_id: 1})
+# video1 = File.open('app/assets/images/test_video.mp4')
+# testvid1.video.attach(io: video1, filename: 'testvid1')
