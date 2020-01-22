f69ef06393c419a0f7d39a3eee1bffddbcb8e602
Mike Madsen <mikewmadsen@gmail.com>
2020-01-22 08:47:36 -0800

    Add userErrorsReducer

--- frontend/reducers/errors/errors_reducer.js
+++ frontend/reducers/errors/errors_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import sessionErrorsReducer from './session_errors_reducer';
 import videoErrorsReducer from './video_errors_reducer';
+import userErrorsReducer from './user_errors_reducer';
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
-  videos: videoErrorsReducer
+  videos: videoErrorsReducer,
+  users: userErrorsReducer
 });
 
 export default errorsReducer;
